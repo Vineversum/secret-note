fc99565da55fb1ac85bd9ea4a4d041aff9d881f4
vineversum <vineversum@protonmail.com>
2023-03-08 19:37:46 +0200

    add copying link to clipboard by click

--- public/script.js
+++ public/script.js
@@ -1,10 +1,23 @@
 const textArea = document.querySelector('textarea');
-const button = document.querySelector('button')
+const button = document.querySelector('button');
+const link = document.querySelector('#link')
 
-textArea.addEventListener('input', (event) => {
-  if (event.target.value.length > 0) {
-    button.disabled = false;
-  } else {
-    button.disabled = true;
-  }
-})
+if(textArea) {
+  textArea.addEventListener('input', (event) => {
+    if (event.target.value.length > 0) {
+      button.disabled = false;
+    } else {
+      button.disabled = true;
+    }
+  })
+}
+
+console.log(link)
+
+if(link) {
+  link.addEventListener('click', async (event) => {
+
+    console.log('CLICKED', event.target.textContent);
+    await window.navigator.clipboard.writeText(event.target.textContent)
+  })
+}
